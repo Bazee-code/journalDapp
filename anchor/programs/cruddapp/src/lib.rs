e0298c29bd4a7f32845f4e7a980204113c57fb77
Bazee-code <eugeneobazee2@gmail.com>
2025-04-04 12:09:28 +0300

    create journal state

--- anchor/programs/cruddapp/src/lib.rs
+++ anchor/programs/cruddapp/src/lib.rs
@@ -8,63 +8,16 @@ declare_id!("coUnmi3oBUtwtd9fjeAvSsJssXh5A5xyPbhpewyzRVF");
 pub mod cruddapp {
     use super::*;
 
-  pub fn close(_ctx: Context<CloseCruddapp>) -> Result<()> {
-    Ok(())
-  }
+    
 
-  pub fn decrement(ctx: Context<Update>) -> Result<()> {
-    ctx.accounts.cruddapp.count = ctx.accounts.cruddapp.count.checked_sub(1).unwrap();
-    Ok(())
-  }
-
-  pub fn increment(ctx: Context<Update>) -> Result<()> {
-    ctx.accounts.cruddapp.count = ctx.accounts.cruddapp.count.checked_add(1).unwrap();
-    Ok(())
-  }
-
-  pub fn initialize(_ctx: Context<InitializeCruddapp>) -> Result<()> {
-    Ok(())
-  }
-
-  pub fn set(ctx: Context<Update>, value: u8) -> Result<()> {
-    ctx.accounts.cruddapp.count = value.clone();
-    Ok(())
-  }
-}
-
-#[derive(Accounts)]
-pub struct InitializeCruddapp<'info> {
-  #[account(mut)]
-  pub payer: Signer<'info>,
-
-  #[account(
-  init,
-  space = 8 + Cruddapp::INIT_SPACE,
-  payer = payer
-  )]
-  pub cruddapp: Account<'info, Cruddapp>,
-  pub system_program: Program<'info, System>,
-}
-#[derive(Accounts)]
-pub struct CloseCruddapp<'info> {
-  #[account(mut)]
-  pub payer: Signer<'info>,
-
-  #[account(
-  mut,
-  close = payer, // close account and return lamports to payer
-  )]
-  pub cruddapp: Account<'info, Cruddapp>,
-}
-
-#[derive(Accounts)]
-pub struct Update<'info> {
-  #[account(mut)]
-  pub cruddapp: Account<'info, Cruddapp>,
 }
 
 #[account]
 #[derive(InitSpace)]
-pub struct Cruddapp {
-  count: u8,
+  pub struct JournalEntryState {
+    pub owner : Pubkey,
+    #[max_len(50)]
+    pub title : String,
+    #[max_len(1000)]
+    pub message : String,
 }
